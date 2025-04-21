73456b7b93622f0eb589bdbaf4692c53d7ddf640
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2025-04-21 17:55:17 +0900

    22

--- infrearn/Hash/5.py
+++ infrearn/Hash/5.py
@@ -1,1 +1,15 @@
-from Collections import Counter
+from collections import Counter
+def solution(S):
+    sH = Counter(S)
+    odd = 0
+    dee = 0
+    for i in sH :
+        if sH[i] % 2 == 1 :
+            odd += 1
+    if odd > 0 :
+        return len(S) - odd + 1
+    else :
+        return len(S)
+            
+
+print(solution("abcbbbccaaeee"))
